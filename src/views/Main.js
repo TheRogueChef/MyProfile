289c84418b87b912fe84b3fdace3a3acd1b0c412
Erik Carlson <theroguechef@comcast.net>
2023-07-12 14:27:28 -0700

    no on the body

--- src/views/Main.js
+++ src/views/Main.js
@@ -11,19 +11,6 @@ import '../components/style.css';
 
 
 const DisplayPage = () => {
-
-    const [width, setWidth] = React.useState(window.innerWidth);
-
-    React.useEffect(() => {
-        const handleWindowResize = () => setWidth(window.innerWidth);
-        window.addEventListener("resize", handleWindowResize);
-        return () => window.removeEventListener("resize", handleWindowResize);
-    }, []);
-
-
-
-
-
     return (
         <div>
             <Element name="top">
