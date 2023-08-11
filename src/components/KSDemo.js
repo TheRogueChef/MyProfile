31ad52bf06d3a6bb59ea981b4b1eab8c04484ff6
Erik Carlson <theroguechef@comcast.net>
2023-08-11 12:49:46 -0700

    Fixing Deploy

--- src/components/KSDemo.js
+++ src/components/KSDemo.js
@@ -6,10 +6,10 @@ import video from './videos/Kitchen_Sync_Demo(v.2).mp4';
 const KSDemo = () => {
     return (
         <div className='DemoBox'>
-            <br /> 
+            <br/> 
             <h1>Kitchen Sync</h1>
             <video  style={{ height: '60%', border: '2px solid silver'}} src={video} controls/>
-            <br /> <br /> 
+            <br/> <br/> 
             <Link to='/'  style={{ color: 'silver', textDecoration: 'none'}}>Home</Link>
         </div>
     )}
