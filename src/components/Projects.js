db23a77edd443f8320e89e83907a1fbccbdcb250
Erik Carlson <theroguechef@comcast.net>
2023-09-01 12:45:01 -0700

    Back to .PNG

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { Image } from 'react-bootstrap';
-import KSLogo from './images/KSLogo.png';
-import MTT from './images/MTT_Logo.png'; 
-import ACLogo from './images/ACLogo.png';
+import KSLogo from './images/KSLogo.PNG';
+import MTT from './images/MTT_Logo.PNG'; 
+import ACLogo from './images/ACLogo.PNG';
 import '../components/style.css';
 
 
@@ -13,7 +13,7 @@ const Projects = () => {
             <h1 className='CTitles'>Projects</h1>
             <br />
             <div className='pBox'>
-                    <Image className='plogo' src={MTT} alt='....' />
+                    <Image className='plogo' src={MTT} alt='...' />
                     <h1 style={{ fontFamily: 'SokaRegular'}}>My Tribe Tracker (In Progress)</h1>
                 <div className='pLinkBox'>
                     <br/>
