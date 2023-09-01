fbd0baf88196476a11f2e569f1e07f923146d38e
Erik Carlson <theroguechef@comcast.net>
2023-09-01 16:26:55 -0700

    added src/type to AC demo

--- src/components/ASDemo.js
+++ src/components/ASDemo.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import video from './videos/AudraSiteDemo.mp4';
 
-
 const ASDemo = () => {
     return (
         <div className='DemoBox'>
             <br /> 
             <h1>Audra Carlson - Stylist Site</h1>
-            <video  style={{ height: '60%', border: '2px solid silver'}} src={video} controls/>
+            <video style={{ height: '60%', border: '2px solid silver' }} controls>
+                <source src={video} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <br /> <br /> 
-            <Link to='/' className='dashBtn' style={{ color: 'white', textDecoration: 'none'}}>Home</Link>
+            <Link to='/' className='dashBtn' style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
         </div>
-    )}
+    );
+}
 
 export default ASDemo;
